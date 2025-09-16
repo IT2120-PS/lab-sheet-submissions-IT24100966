73bc4d8a6f02985fc1306a961847b3547bbdabd9
Pamith Kaluarachchi <it24100966@my.sliit.lk>
2025-09-16 20:37:22 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24100966.R
@@ -0,0 +1,14 @@
+setwd("/Users/pamithkaluarachchi/SLIIT/Y2S1/PS/Labs/Lab 7/IT24100966")
+getwd()
+
+# Q1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+# Q2
+pexp(2, rate = 1/3)
+
+# Q3 (i)
+1 - pnorm(130, mean = 100, sd = 15)
+
+# Q3 (ii)
+qnorm(0.95, mean = 100, sd = 15)
