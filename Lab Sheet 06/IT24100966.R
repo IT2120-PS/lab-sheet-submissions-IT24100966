1ec71c5eb14131e4439311024f12f40523454d96
Pamith Kaluarachchi <it24100966@my.sliit.lk>
2025-09-19 09:48:02 +0530

    Add Lab 6

---
+++ Lab Sheet 06/IT24100966.R
@@ -0,0 +1,18 @@
+#1
+setwd("/Users/pamithkaluarachchi/SLIIT/Y2S1/PS/Labs/Lab 6/IT24100966")
+#i
+#Binomial Distribution
+
+#ii
+1 - pbinom(46, 50, 0.85, lower.tail = TRUE)
+pbinom(46, 50, 0.85, lower.tail = FALSE)
+
+#2
+#i
+# Number of customer calls received per hour
+
+#ii
+# Poisson distribution
+
+#iii
+dpois(15, 12)
